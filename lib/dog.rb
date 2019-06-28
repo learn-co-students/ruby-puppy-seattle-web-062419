bce1c3257ca6a46aebbbbedd8610aed3d2d784a2
aminamos <26092352+aminamos@users.noreply.github.com>
2019-06-28 15:00:24 -0700

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,26 @@
-# Add your code here
+class Dog
+
+    @@all = []
+
+    def initialize
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.print_all
+        @@all.each do |x|
+            puts x
+        end
+    end
+
+    def self.clear_all
+        @@all.clear
+    end
+
+
+
+
+end
